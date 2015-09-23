1d71ef1e70274e7a37e98df92d78754a1738c1a5
rajan357 <rajan.nikhi@gmail.com>
2015-09-23 17:19:33 -0500

    Added StringTokenizer class

---
+++ Demo/src/com/mypackage/StringTokenizerDemo.java
@@ -0,0 +1,41 @@
+package com.mypackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringTokenizerDemo {
+
+	private static Scanner sc;
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the pattern to separate with delimiter");
+		String str=br.readLine();
+		StringTokenizer st = new StringTokenizer(str, ",");
+		Pattern p=Pattern.compile("19*[0-9]");
+		System.out.println(st.countTokens());
+		int count = 1;
+		boolean b=false;
+		while (st.hasMoreTokens()) {
+			String str1= st.nextToken();
+			Matcher m=p.matcher(str1);
+			 b=m.matches();
+			if(b==true)
+			{
+				int x=Integer.parseInt(str1);
+				int y=(x+10);
+				System.out.println("token number" +count+ " "  +y);
+			}
+			else
+		  System.out.println("The token:"  +count+ " "+str1);	
+			count++;
+		}
+
+	}
+
+}
